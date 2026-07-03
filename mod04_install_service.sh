9770a0c74c11fd2a3ead52dffc94bd20e36a7447
github19999 <151017823+github19999@users.noreply.github.com>
2026-07-03 08:49:56 +0800

    Update mod04_install_service.sh

--- mod04_install_service.sh
+++ mod04_install_service.sh
@@ -982,7 +982,7 @@ menu_install_service() {
             SVC_CHOICES=(1 4 7 10 14 160 17)
             AUTO_DEFAULT=true
         elif [[ "$vc_raw" == "101" ]]; then
-            SVC_CHOICES=(1 4 7 10 14 160 17)
+            SVC_CHOICES=(1 4 7 10 14 160)
             AUTO_DEFAULT=false
         elif [[ "$vc_raw" == "102" ]]; then
             read -rp "请输入服务编号（例如 1 4 7，以空格隔开）: " -a SVC_CHOICES
